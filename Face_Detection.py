59d17183cfe781ff1ac7cd03f2589a90fb9ab480
Jyoti Mishra <mishranishi02@gmail.com>
2021-02-03 15:58:22 +0530

    detecting face

--- Face_Detection.py
+++ Face_Detection.py
@@ -1,15 +1,23 @@
-import cv2 as cv
+import cv2
 
 # load some pre-trained data on face frontals from opencv (haar cascade algorithm)
-trained_face_data = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+# trained_face_data = cv2.CascadeClassifier('D:\\PycharmProjects\\firstProg\\face detection\\haarcascade_frontface_default.xml')
+trained_face_data = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 # choose an image to detect faces in
-img = cv.imread('chris.jfif')
+img = cv2.imread('chris.jpg')
 
 # converting to grayscale
-grayscaled_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-cv.imshow('Chris Hemsworth', grayscaled_img)
-cv.waitKey()
+# Detect faces
+face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+# draw rectangles around the faces
+cv2.rectangle(img, (271,100), (452, 281), (0,255,0), 2)
+
+print(face_coordinates)
+cv2.imshow('Chris Hemsworth', img)
+cv2.waitKey()
 
 print("Code Completed")
