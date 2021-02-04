7657c0e123398391a70621abb0f3cf0cbc986416
Jyoti Mishra <mishranishi02@gmail.com>
2021-02-04 20:48:37 +0530

    detecting multiple faces

--- Face_Detection.py
+++ Face_Detection.py
@@ -1,11 +1,12 @@
 import cv2
+from random import randrange
 
 # load some pre-trained data on face frontals from opencv (haar cascade algorithm)
-# trained_face_data = cv2.CascadeClassifier('D:\\PycharmProjects\\firstProg\\face detection\\haarcascade_frontface_default.xml')
 trained_face_data = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 # choose an image to detect faces in
-img = cv2.imread('chris.jpg')
+img = cv2.imread('Chris_Hemsworth.jpg')
+# img = cv2.imread('chris.jpg')
 
 # converting to grayscale
 grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -13,10 +14,15 @@ grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # Detect faces
 face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
 
-# draw rectangles around the faces
-cv2.rectangle(img, (271,100), (452, 281), (0,255,0), 2)
+# draw rectangles around a single face
+# (x,y,w,h) = face_coordinates[0]
+# cv2.rectangle(img, (x, y), (x + w, y + h), (randrange(256), randrange(256), randrange(256)), 2)
 
-print(face_coordinates)
+# draw rectangle around multiple faces
+for (x,y,w,h) in face_coordinates:
+    cv2.rectangle(img, (x,y), (x+w, y+h), (randrange(256),randrange(256),randrange(256)), 2)
+
+# print(face_coordinates)
 cv2.imshow('Chris Hemsworth', img)
 cv2.waitKey()
 
